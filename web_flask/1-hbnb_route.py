8338f17e9bd9c2c174d874f910e64a2a173d7aae
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-31 05:18:51 -0700

    display “Hello HBNB!”

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Flask web application
-python web framework
 """
+Flask web application
+"""
+
 
 from flask import Flask
 
-
 app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def welcome():
+    """A function that display Hello HBNB"""
+    return 'Hello HBNB!'
 
 
 @pp.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    A function that display “HBNB”
-    """
-    return "HBNB"
+    A function that display “HBNB”"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
