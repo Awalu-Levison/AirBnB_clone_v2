76282c32804205d5a8cc271bb124aa160a9b476e
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-30 23:03:19 -0700

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Flask web application
+python web framework
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def welcome():
+    """A function that execute code
+    for home page
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port:5000)
