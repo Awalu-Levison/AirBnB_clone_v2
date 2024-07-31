49ada744896216d6a4c369f5bb8f617dd6fc18cc
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-31 04:26:08 -0700

    Display HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Flask web application
+python web framework
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def welcome():
+    """A function that execute code
+    for home page
+    """
+    return 'Hello HBNB!'
+
+
+@pp.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    A function that display “HBNB”
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
