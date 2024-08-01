3d280bec35e611f35f88aecbff002e0d9525c57f
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-31 17:56:36 -0700

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,8 +23,8 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """work with c parameters"""
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 
